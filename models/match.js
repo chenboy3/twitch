9ab2072d704399addfd4585a05a446c1b1c4cab7
Victor <victorc1165@yahoo.com>
2016-10-01 13:30:12 -0700

    centered, cleared unecessary files

--- models/match.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	Player = require('./player');
-
-var Matches = new Schema({
-	pair: [Player]
-});
-module.exports = Matches;
--- models/player.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
-
-var PlayerSchema = new Schema({
-	name: {type: String},
-	number: {type: String},
-	playerId: {type: Number}
-});
-module.exports = PlayerSchema;
